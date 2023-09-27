f587ada4d3c445bb35d6d794f2311635ed4cd99b
jenraii <131862365+jenraii@users.noreply.github.com>
2023-09-27 14:09:46 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './App.css'
+
+const App = () => {
+const [todo, setTodo] = useState({date: '', description: ''});
+const [todos, setTodos] = useState([]);
+
+const inputChanged = (event) => {
+  setTodo({...todo, [event.target.name]: event.target.value });
+}
+
+const addTodo = (event) => {
+  event.preventDefault();
+  setTodos([...todos, todo]);
+  setTodo({ date: '', description: '' });
+}
+
+  return (
+    <div className="App">
+    <header>
+      <h1>Simple Todolist</h1>
+    </header>
+    <main>
+      <form onSubmit={addTodo}>
+        <fieldset>
+          <legend>Add todo:</legend>
+          <label>Date:
+            <input type="text" name="date" onChange={inputChanged} value={todo.date} />
+          </label>
+          <label>Description:
+            <input type="text" name="description" onChange={inputChanged} value={todo.description} />
+          </label>
+          <input type="submit" value="Add" />
+        </fieldset>
+      </form>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map((todo, index) => <tr key={index}>
+          <td>{todo.date}</td>
+          <td>{todo.description}</td>
+          </tr>)}
+      </tbody>
+    </table>
+    </main>
+    </div>
+  );
+};
+
+export default App
